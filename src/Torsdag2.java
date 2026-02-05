b37db2fda5567acc70266173385ca70f8c20688d
Mahdi Al-Dailemi <mahdiald.2003@gmail.com>
2026-02-05 11:14:05 +0100

    4

--- src/Torsdag2.java
+++ src/Torsdag2.java
@@ -1,4 +1,42 @@
-void main(){
-    int i = 0;
-    System.out.println(i);
+void main() {
+// opgave 1
+/*
+    double totalPrice = 1200;
+    double finalPrice = 0;
+    if (totalPrice>1000){
+        finalPrice = totalPrice*0.80;
+        System.out.println(finalPrice + " Discount applied");
+    }else{System.out.println(totalPrice);}
+*/
+
+//Opgave 2
+    /*
+    double weight = 120; //kg
+    double height = 1.60; //meter
+    double BMI = weight/(height * height);
+    if (BMI >= 25) {
+        System.out.println("Over vægtig");
+    } else {
+        System.out.println("Ikke overvægtig");
+
+
+    }
+*/
+
+//opgave 3
+  /*
+    int hour = 14;
+    if (hour>=5 && hour<=11){
+        System.out.println("Good morning");
+    } else if (hour>=12 && hour<=17) {
+        System.out.println("Good evening");
+    } else if (hour>=18 && hour<=21) {
+        System.out.println("Good afternoon");
+    }else {
+        System.out.println("Good night");
+    }
+    */
+
+//opgave 4
+
 }
